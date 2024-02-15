19a9fc05a2fe2bed1dcbedf43baf9abc14dce5bd
Prince Prakalp <30505797+algorithm98@users.noreply.github.com>
2024-02-16 03:17:06 +0530

    Fiest Comit pmt

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/techsolutionstechnologies/Desktop/next/pmt/app/layout.jsx": [
+      "/Users/techsolutionstechnologies/Desktop/next/pmt/styles/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/techsolutionstechnologies/Desktop/next/pmt/app/page": [
+      "/Users/techsolutionstechnologies/Desktop/next/pmt/styles/globals.css"
+    ]
+  }
+}
